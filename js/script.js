22c37bb81228e715d656404fc606972a3df83cc1
hhong0302 <wonhong0302@naver.com>
2023-07-11 00:52:24 +0900

    s

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 const ham = document.getElementById("hamburger");
+const navbar = document.getElementById("navbar");
 let ham_off = true;
 function hamclick()
 {
     if(ham_off)
     {
+        navbar.classList.add("active");
         ham.innerHTML=`<i class="fa-solid fa-xmark"></i>`;
         ham_off=false;
     }
     else
     {
+        navbar.classList.remove("active");
         ham.innerHTML=`<i class="fa-solid fa-bars"></i>`;
         ham_off=true;
     }
